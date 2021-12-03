cb431ebc010660b31eb80cee2b5b5c8fe2c0cac2
Malou Oberhansberg <malouob@googlemail.com>
2021-12-03 17:33:53 +0000

    classification model

--- small_business/malou_translator.py
+++
@@ -1,9 +0,0 @@
-from google_trans_new import google_translator
-import pandas as pd
-#note source code in google_trans_new needs to be updated. Ask me how!
-
-def translate(df):
-    for i in df.Review:
-        translator = google_translator()
-        i = translator.translate(i, lang_src = "auto", lang_tgt = "en")
-        df.Review[i] = i
--- translator.py
+++
@@ -1,7 +0,0 @@
-from deep_translator import GoogleTranslator
-from deep_translator import single_detection , batch_detection
-
-lang = batch_detection(['bonjour la vie', 'hello world'], api_key='your_api_key')
-print(lang) # output: [fr, en]
-
-translated = GoogleTranslator(source='auto', target=lang).translate(text=text)
