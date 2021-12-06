4e39c0f9a3a5759c42c4d7a67a3ab1044372fda6
Malou Oberhansberg <malouob@googlemail.com>
2021-12-06 19:17:08 +0000

    fixed model

--- small_business/m_get_restaurants_data.py
+++ small_business/m_get_restaurants_data.py
@@ -4,6 +4,6 @@ def restaurant_data():
 
     data = pd.read_csv('../small_business/data/restaurants.csv')
     data = data.drop(columns = 'Unnamed: 0')
-    X = data.drop(columns=['rating','name', 'address', 'label', 'postal_code', 'no_del_exp', 'municipality', 'review_count'])
+    X = data.drop(columns=['rating','name', 'address', 'label', 'curb_pickup', 'drive_through', 'postal_code', 'no_del_exp', 'municipality', 'review_count'])
     y = data['rating']
     return X, y
