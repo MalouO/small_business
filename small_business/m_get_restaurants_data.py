92ed8973df6305875af4a44025629b68c00abe88
Malou Oberhansberg <malouob@googlemail.com>
2021-12-07 12:17:08 +0000

    updated prediction model

--- small_business/m_get_restaurants_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def restaurant_data():
-
-    data = pd.read_csv('../small_business/data/restaurants.csv')
-    data = data.drop(columns = 'Unnamed: 0')
-    X = data.drop(columns=['rating','name', 'address', 'label', 'curb_pickup', 'drive_through', 'postal_code', 'no_del_exp', 'municipality', 'review_count'])
-    y = data['rating']
-    return X, y
